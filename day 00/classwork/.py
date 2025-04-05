bca04a1d0651542f192b01afd315817f9e6dbbe6
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-04-05 20:55:22 +0400

    second commit

--- day 00/classwork/.py
+++ day 00/classwork/.py
@@ -1,1 +1,23 @@
-#eidm
+#str-"რაც ბრჭყალებში იწერება" 
+#int-მთელი მთლიანი რიცხვები
+#float-რიცხვები როგორიცაა მაგ:5.68
+#boolean-true or false
+
+#num = "string"
+#num2 = (5)
+#num3 = (5.69)
+#num4 = ("true")
+
+
+#my_variable = 10
+#my_variable = float(my_variable)
+#print(my_variable)
+
+# = 15
+#my_age = int(input("your age:"))
+
+#print(age - my_age)
+
+#user = (input("enter your age:"))
+#user1 = int(input("enter your num:"))
+#print(user * user1)
